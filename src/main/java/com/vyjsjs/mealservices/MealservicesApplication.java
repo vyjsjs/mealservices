a238f914f5900b14ac1672635b071cb9be6ab39d
vyjsjs <59689357+vyjsjs@users.noreply.github.com>
2025-09-26 12:22:05 +0900

    feat: Initial Spring Boot project setup and configuration

---
+++ src/main/java/com/vyjsjs/mealservices/MealservicesApplication.java
@@ -0,0 +1,20 @@
+package com.vyjsjs.mealservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 간단 식단 관리 웹 서비스의 메인 애플리케이션 클래스입니다.
+ * Spring Boot 애플리케이션을 실행하는 역할을 합니다.
+ */
+
+@SpringBootApplication
+public class MealservicesApplication {
+	public static void main(String[] args){
+		// 애플리케이션 실행
+		SpringApplication.run(MealservicesApplication.class, args);
+
+		// 서버가 정상적으로 시작되면 콘솔에 메시지가 출력됩니다.
+		System.out.println("--- Meal Services Started Successfully! ---");
+	}
+}
---
+++ src/test/java/com/vyjsjs/mealservices/MealservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vyjsjs.mealservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MealservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
